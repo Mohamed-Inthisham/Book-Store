ec2cb1cd655858f376fe3f0ae783de9c6d2723b4
Mohamed-Inthisham <balck.devil9b@gmail.com>
2023-12-03 13:30:09 +0530

    deleted specific book

--- src/main/java/com/example/ProjectBookStore/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/ProjectBookStore/service/impl/BookServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.ProjectBookStore.service.impl;
 import com.example.ProjectBookStore.controller.request.BookRequest;
 import com.example.ProjectBookStore.service.BookService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Service
 public class BookServiceImpl implements BookService {
@@ -10,5 +11,11 @@ public class BookServiceImpl implements BookService {
     @Override
     public void create(BookRequest request) {
         System.out.println("Book created : "+request.getTitle());
+    }
+
+    @Override
+    public void delete(BookRequest request) {
+        System.out.println("Deleted Book Name : "+request.getTitle());
+
     }
 }
